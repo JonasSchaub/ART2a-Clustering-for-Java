a47fd154a51461cbc4bc4466f8e39b27d71340b7
Jonas Schaub <44881147+JonasSchaub@users.noreply.github.com>
2023-07-03 18:27:47 +0200

    Another adjustment of license headers

--- src/main/java/de/unijena/cheminf/clustering/art2a/results/Art2aFloatClusteringResult.java
+++ src/main/java/de/unijena/cheminf/clustering/art2a/results/Art2aFloatClusteringResult.java
@@ -1,11 +1,8 @@
 /*
  * ART2a Clustering for Java
- *
  * Copyright (C) 2023 Betuel Sevindik, Felix Baensch, Jonas Schaub, Christoph Steinbeck, and Achim Zielesny
  *
  * Source code is available at <https://github.com/JonasSchaub/ART2a-Clustering-for-Java>
- *
- * GNU General Public License v3.0
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -14,11 +11,11 @@
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 package de.unijena.cheminf.clustering.art2a.results;
